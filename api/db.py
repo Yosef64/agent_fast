230d073817f58da0b6220df803937c3bb135ac9e
Yosef64 <yosefale65@gmail.com>
2024-11-18 23:01:07 +0300

    changing key.json into base64

--- api/db.py
+++ api/db.py
@@ -1,7 +1,13 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
+import os
+import json
+import base64
+from firebase_admin import credentials,firestore
 
-cred = credentials.Certificate("key.json")
-firebase_admin.initialize_app(cred,{'storageBucket': 'victorybot-bc6db.appspot.com'})
+firebase_key_base64 = os.getenv("FIREBASE_KEY_BASE64")
+if firebase_key_base64:
+    firebase_key_json = json.loads(base64.b64decode(firebase_key_base64).decode('utf-8'))
+    cred = credentials.Certificate(firebase_key_json)
+else:
+    raise ValueError("FIREBASE_KEY_BASE64 environment variable not set")
 
 db = firestore.client()
