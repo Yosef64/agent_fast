5b3d382268fddd00f2e70c531428517eafc1a672
Yosef64 <yosefale65@gmail.com>
2024-11-18 21:57:58 +0300

    adding some features

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Application, CommandHandler
+from telegram.ext import Application, CommandHandler,MessageHandler,CallbackQueryHandler,filters,CallbackContext
 from telegram.ext._contexttypes import ContextTypes
 from fastapi import FastAPI, Request, Response
 
@@ -10,6 +10,30 @@ async def start(update, _: ContextTypes.DEFAULT_TYPE):
     keyboard = [[InlineKeyboardButton("Visit Web App", web_app={"url": "https://victory-contest.vercel.app/"})]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("Welcome! Click the button below to visit our web app.", reply_markup=reply_markup)
+async def options(update,_,context:CallbackContext):
+    text = update.message.text
+    # if text == "🔢 በኤጀንቶቻቹ የገቡ የተማሪዎች ብዛት":
+    #     await 
+    # elif text == "📝 ለኤጀንት ምዝገባ":
+    #     await register(update,context)
+    # elif text == "💰 ብር ለማውጣት":
+    #     await payMe(update,context)
+    # elif text == "🔗 Referral link ለማግኘት":
+    #     await getLinks(update,context)
+    # elif text == "💵 ቀሪ ሂሳብ ለማወቅ":
+    #     await getAmount(update,context)
+    # elif text == "📊 በስራቹ የገቡ የተማሪዎች ብዛት":
+    #     await getNumberOfStud(update,context)
+    # elif text == "በስራቹ የተመዘገቡ ኤጀንቶች ብዛት":
+    #     await ownAgent(update,context)
+    await update.message.reply_text(text)
+# async def button(update:_,context:CallbackContext):
+#     query = update.callback_query
+#     await query.answer()
+#     if query.data == "1":
+#         await query.
+#     elif query.data == "2":
+#         await getStudentReferral(update,context)
 @app.post("/")
 async def process_update(request: Request):
     ptb = (
@@ -22,6 +46,8 @@ async def process_update(request: Request):
    )
     await ptb.initialize()
     ptb.add_handler(CommandHandler("start", start))
+    ptb.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND,options))
+    # ptb.add_handler(CallbackQueryHandler(button))
     req = await request.json()
     
     update = Update.de_json(req, ptb.bot)
