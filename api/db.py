c6fe81137bc62ddc9e62127744ff7d3638ee2627
Yosef64 <yosefale65@gmail.com>
2024-11-18 23:04:46 +0300

    adding initializing app

--- api/db.py
+++ api/db.py
@@ -1,6 +1,7 @@
 import os
 import json
 import base64
+import firebase_admin
 from firebase_admin import credentials,firestore
 
 firebase_key_base64 = os.getenv("FIREBASE_KEY_BASE64")
@@ -9,5 +10,5 @@ if firebase_key_base64:
     cred = credentials.Certificate(firebase_key_json)
 else:
     raise ValueError("FIREBASE_KEY_BASE64 environment variable not set")
-
+firebase_admin.initialize_app(cred)
 db = firestore.client()
